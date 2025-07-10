b4f9ece213cf328b5ec5374fea46dc6baa079aaa
Rockey <rkrockey9690@gmail.com>
2025-07-10 19:19:19 +0530

    commit

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -27,7 +27,8 @@ const signup = () => {
        const handleUserData = async (userData) => {
         console.log("user", userData)
          try {
-          const response = await axios.post("https://libraryserver-2.onrender.com/api/signup", userData)
+          const response = await axios.post("https://libraryserver-2.onrender.com/api/signup", userData, {withCredentials: true,headers: {" Content-Type": "application/json"}
+});
            console.log("userData", response.data)
            toast.success("SignUp Successfully")
             navigate('/')
